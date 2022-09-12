ceb47cef4c23d34ce4bf9ee5339ec5ee51ffa2fb
MZubairLohar <zbrlohar@gmail.com>
2022-09-12 18:26:18 +0500

    basic structure

--- components/UseAnimation.tsx
+++ components/UseAnimation.tsx
@@ -1,13 +1,22 @@
+import React,{ useRef } from 'react';
 import {  useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import {  BoxHelper } from 'three';
+import { useHelper } from '@react-three/drei';
 
-const UseAnimation = ()=>{
+
+type Props = {
+    isTesting: boolean;
+}
+
+const UseAnimation:  React.FC<Props> = ({  isTesting })=>{
     const myRef = useRef<THREE.Mesh>(null)
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    {isTesting ? useHelper(myRef, BoxHelper , "blue") : null }
+    // useHelper(myRef, BoxHelper , "blue")
     useFrame(()=>{
-        myRef.current?.rotation.x += 0.1;
-        myRef.current?.rotation.y +=0.1;        
-        myRef.current?.rotation.z +=0.1;
-
+        if(myRef.current){
+            myRef.current.rotation.x += 0.01;
+        }
     });
     return(
         <mesh ref={myRef} scale={[0.5,0.5,0.5]}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,29 @@
 import type { NextPage } from 'next'
-import UseAnimation from '../components/UseAnimation';
 import { Canvas } from '@react-three/fiber';
-
+import { OrbitControls, Stats } from '@react-three/drei';
+// import CameraOrbitController from '../components/CameraOrbitController'
+// import UseAnimation from '../components/UseAnimation';
 
 const Home: NextPage = () => {
+
+  const testing = true;
   
   return (
     <div className="container">
-      <Canvas >
+      <Canvas camera={{fov : 75, near: 2, far : 100}}>
+          <OrbitControls />
+          {/* {testing? <Stats /> : null} */}
+          {testing? <axesHelper args = {[2]} /> : null}
+          {testing?  <gridHelper args = {[2, 10]} />: null }
+
+          {/* <CameraOrbitController/> */}
           <ambientLight intensity={[0.1]} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-          <UseAnimation />
+          <directionalLight color="blue" position={[0, 0, 5]} />
+          {/* <UseAnimation isTesting ={testing} /> */}
+          <mesh  scale={[0.5,0.5,0.5]}>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
       </Canvas>
     </div>
   )
